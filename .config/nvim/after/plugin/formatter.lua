93da2fd12209af0d7d5bbce292999350dd2e3f2a
Joinemm <joonas@rautiola.co>
2023-02-10 14:41:11 +0200

    update config

--- .config/nvim/after/plugin/formatter.lua
+++ .config/nvim/after/plugin/formatter.lua
@@ -9,21 +9,18 @@ require("formatter").setup({
 	logging = true,
 	log_level = vim.log.levels.WARN,
 	filetype = {
-		lua = {
-			require("formatter.filetypes.lua").stylua,
-		},
-		javascript = {
-			require("formatter.filetypes.javascript").prettier,
-		},
+		lua = { require("formatter.filetypes.lua").stylua },
 
-		python = {
-			require("formatter.filetypes.python").black,
-		},
+		javascript = { require("formatter.filetypes.javascript").prettier },
 
-		-- Use the special "*" filetype for defining formatter configurations on any filetype
-		["*"] = {
-			-- "formatter.filetypes.any" defines default configurations for any filetype
-			require("formatter.filetypes.any").remove_trailing_whitespace,
-		},
+		python = { require("formatter.filetypes.python").black },
+
+		sh = { require("formatter.filetypes.sh").shfmt },
+
+		nix = { require("formatter.filetypes.nix").alejandra },
+
+		rust = { require("formatter.filetypes.rust").rustfmt },
+
+		["*"] = { require("formatter.filetypes.any").remove_trailing_whitespace },
 	},
 })
